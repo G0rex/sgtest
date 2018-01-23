c60f40b357d4937860f48952964883a79fa04b58
ihor <morgunov.igor1990@gmail.com>
2018-01-23 11:14:50 +0200

    initialisation

---
+++ app/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ihor
+ * Date: 23.01.18
+ * Time: 11:02
+ */
+echo 'hello';exit;
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ihor
+ * Date: 23.01.18
+ * Time: 11:09
+ */
+require_once 'app/index.php';
